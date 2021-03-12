c68fd63991fa4bfd83cea92e2bd01a9a13153021
parthpanchal <parth.panchal@softqubes.com>
2021-03-12 19:53:28 +0530

    implement svg feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
 import "./App.css";
+import { SketchPicker } from "react-color";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const defaultColor = "#000";
+  const [color, updateColor] = useState(defaultColor);
+  const [demoText, updateDemoText] = useState("");
+
+  const handleOnChangeComplete = (color) => {
+    updateColor(color.hex);
+  };
+
+  const handleDemoText = (e) => {
+    console.log(e.target.value);
+    updateDemoText(e.target.value);
+  };
+
+  const rectStyle = {
+    fill: color,
+  };
+
+  const textStyle = {
+    fill: "#fff",
+    fontSize: "14",
+    fontFamily: "Verdana",
+  };
+
+  return (
+    <div>
+      <div className="demo-text">
+        <label htmlFor="demo-text">Demo Text:</label>
+        <input
+          type="text"
+          id="demo-text"
+          name="demo-text"
+          onChange={handleDemoText}
+        ></input>
+      </div>
+      <SketchPicker color={color} onChangeComplete={handleOnChangeComplete} />
+      <svg xmlns="http://www.w3.org/2000/svg">
+        <g>
+          <rect x="0" y="0" width="1000" height="1000" style={rectStyle}></rect>
+          <text x="2" y="20" style={textStyle}>
+            {demoText}
+          </text>
+        </g>
+      </svg>
+    </div>
+  );
 }
 
 export default App;
